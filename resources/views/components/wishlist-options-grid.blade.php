46664cd43633c5b2de474786dc952e3475ca602b
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2022-12-10 20:44:25 +0100

    add rounded and make text gray to better fit original design

--- resources/views/components/wishlist-options-grid.blade.php
+++ resources/views/components/wishlist-options-grid.blade.php
@@ -1,15 +1,15 @@
 <div class="bg-gray-200 p-4">
-    <div class="grid grid-cols-3 gap-4">
-        <div class="bg-blue-300 p-4 text-center">
+    <div class="grid grid-cols-3 gap-4 text-gray-50 uppercase font-semibold">
+        <div class="bg-blue-300 p-4 text-center rounded-xl">
             Del
         </div>
-        <div class="bg-blue-300 p-4 text-center">
+        <div class="bg-blue-300 p-4 text-center rounded-xl">
             Rediger
         </div>
-        <div class="bg-blue-300 p-4 text-center">
+        <div class="bg-blue-300 p-4 text-center rounded-xl">
             Slet
         </div>
-        <div class="bg-blue-300 p-4 col-span-3 text-center">
+        <div class="bg-blue-300 p-4 col-span-3 text-center rounded-xl">
             Opret ønske
         </div>
     </div>
